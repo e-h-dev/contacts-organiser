c9bea1559a16606a577d0e658ef8f6b3fb0d64b9
Eli Hammond <ebaal8226@gmail.com>
2024-09-24 13:36:27 +0000

    Wire up atadbase to html templateates

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
+from .models import Item
 from .form import FormContactItem
 
 # Create your views here.
 
+# def index(request):
+#     """ Aview to return index page """
+#     return render(request, 'home/index.html')
+
+
 def index(request):
-    """ Aview to return index page """
-    return render(request, 'home/index.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'home/index.html', context)
 
 
 def add_contact(request):
