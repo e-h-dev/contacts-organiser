c5871004ff11513f06d5d8f5a344349f23f4b3e0
Eli Hammond <ebaal8226@gmail.com>
2024-09-25 13:59:17 +0000

    Update css

--- contacts_organiser/settings.py
+++ contacts_organiser/settings.py
@@ -152,7 +152,8 @@ APPEND_SLASH = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
